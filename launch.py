ae2211e4c980aeddf7e730e7eb1fece13a4e3c8b
nuclearavocado <32930305+nuclearavocado@users.noreply.github.com>
2020-03-26 11:35:45 +0000

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,7 +1,4 @@
 import gym
-
-# import gym_EURUSD
-
 # Local
 import models
 import policies
@@ -20,19 +17,6 @@ def policy_gradient(env, env_params):
         args.policy = 'Gaussian'
     logger = loggers.TensorBoardLogger(args)
     agent = reinforce.REINFORCE(args, env, env_params, logger, buffer, policy, model)
-    return agent
-
-def policy_gradient_image(env, env_params):
-    from agents import reinforce_image
-    buffer = replay_buffer.PolicyBuffer()#env_params, args.buffer_size)
-    model = models.CNN(env_params)
-    if env_params['discrete_env']: # Discrete
-        policy = policies.CategoricalPolicy
-    else: # Continuous
-        policy = policies.GaussianPolicy
-        args.policy = 'Gaussian'
-    logger = loggers.TensorBoardLogger(args)
-    agent = reinforce_image.REINFORCE(args, env, env_params, logger, buffer, policy, model)
     return agent
 
 def actor_critic(env, env_params):
@@ -63,34 +47,6 @@ def vpg(env, env_params):
     agent = vpg.VPG(args, env, env_params, logger, buffer, policy, actor, critic)
     return agent
 
-def npg(env, env_params):
-    from agents import npg
-    buffer = replay_buffer.ActorCriticBuffer()
-    actor = models.Actor(env_params)
-    critic = models.Critic(env_params)
-    if env_params['discrete_env']: # Discrete
-        policy = policies.CategoricalPolicy
-    else: # Continuous
-        policy = policies.GaussianPolicy
-        args.policy = 'Gaussian'
-    logger = loggers.TensorBoardLogger(args)
-    agent = npg.NPG(args, env, env_params, logger, buffer, policy, actor, critic)
-    return agent
-
-def ppo(env, env_params):
-    from agents import ppo
-    buffer = replay_buffer.ActorCriticBuffer()
-    actor = models.Actor(env_params)
-    critic = models.Critic(env_params)
-    if env_params['discrete_env']: # Discrete
-        policy = policies.CategoricalPolicy
-    else: # Continuous
-        policy = policies.GaussianPolicy
-        args.policy = 'Gaussian'
-    logger = loggers.TensorBoardLogger(args)
-    agent = ppo.PPO(args, env, env_params, logger, buffer, policy, actor, critic)
-    return agent
-
 def main(args):
     env = gym.make(args.env_name)
     # env = gym.make('EURUSD-v0')
@@ -99,25 +55,12 @@ def main(args):
     if args.algo == 'reinforce':
         ''' Policy Gradient '''
         agent = policy_gradient(env, env_params)
-    elif args.algo == 'reinforce-image':
-        ''' Policy Gradient '''
-        agent = policy_gradient_image(env, env_params)
     elif args.algo == 'actor-critic':
         ''' Actor-Critic '''
         agent = actor_critic(env, env_params)
     elif args.algo == 'vpg':
         ''' Vanilla Policy Gradient '''
         agent = vpg(env, env_params)
-    elif args.algo == 'npg':
-        ''' Natural Policy Gradient '''
-        agent = npg(env, env_params)
-    elif args.algo == 'trpo':
-        ''' Trust Region Policy Optimization '''
-        # agent = trpo(env, env_params)
-        pass
-    elif args.algo == 'ppo':
-        ''' Proximal Policy Optimization '''
-        agent = ppo(env, env_params)
 
     agent.train()
 
